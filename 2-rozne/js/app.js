c973e2a6d7a9b26890aaa7124dc1b5acdd4d11f0
Meikelele <michal.bak2002@interia.pl>
2025-05-28 13:38:11 +0200

    S02E02 - done

--- 2-rozne/js/app.js
+++ 2-rozne/js/app.js
@@ -1,18 +1,39 @@
-const input = prompt("Podaj jakis input...");
+// {
+//     const input = prompt("Podaj jakis input...");
+    
+//     function countWord(input) {
+//         if (!input) {
+//             return null;
+//         }
+    
+//         const count = input.trim().split(" ").length;
+//         return count;
+//     }
+    
+//     const count = countWord(input);
+    
+//     if (count === null) {
+//         console.log("Nie mam co liczyć");
+//       } else {
+//         console.log(`Tekst "${input}" składa się z ${count} ${count === 1 ? "wyrazu" : "wyrazów"}`);
+//       }
+// }
 
-function countWord(input) {
-    if (!input) {
-        return null;
-    }
+const userName = prompt("Podaj swoje imie...");
 
-    const count = input.trim().split(" ").length;
-    return count;
+function fixName(userName) {
+    if (!userName || !userName.trim()) return null;
+    // let userNameLowerCase = userName.toLowerCase();
+    // let firstUpperChar = userName.substring(0,1).toUpperCase();
+    // return firstUpperChar + userNameLowerCase.slice(1);
+
+    userName = userName.toLowerCase();
+    return userName[0].toUpperCase() + userName.slice(1);
+
+
 }
+const userNameFixed = fixName(userName);
 
-const count = countWord(input);
+if (userNameFixed === null) { console.log(`nic nie wpisano`) }
+else { console.log(`Imie ${userNameFixed} rozpoczyna sie od litery ${userNameFixed[0]}`); }
 
-if (count === null) {
-    console.log("Nie mam co liczyć");
-  } else {
-    console.log(`Tekst "${input}" składa się z ${count} ${count === 1 ? "wyrazu" : "wyrazów"}`);
-  }

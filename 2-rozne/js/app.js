ad532c98bef7ebcf34d106fd9afb58263bd6d78c
Meikelele <michal.bak2002@interia.pl>
2025-05-30 12:56:35 +0200

    S02E03 - done

--- 2-rozne/js/app.js
+++ 2-rozne/js/app.js
@@ -19,21 +19,41 @@
 //       }
 // }
 
-const userName = prompt("Podaj swoje imie...");
+// {const userName = prompt("Podaj swoje imie...");
 
-function fixName(userName) {
-    if (!userName || !userName.trim()) return null;
-    // let userNameLowerCase = userName.toLowerCase();
-    // let firstUpperChar = userName.substring(0,1).toUpperCase();
-    // return firstUpperChar + userNameLowerCase.slice(1);
+// function fixName(userName) {
+//     if (!userName || !userName.trim()) return null;
+//     // let userNameLowerCase = userName.toLowerCase();
+//     // let firstUpperChar = userName.substring(0,1).toUpperCase();
+//     // return firstUpperChar + userNameLowerCase.slice(1);
 
-    userName = userName.toLowerCase();
-    return userName[0].toUpperCase() + userName.slice(1);
+//     userName = userName.toLowerCase();
+//     return userName[0].toUpperCase() + userName.slice(1);
 
 
+// }
+// const userNameFixed = fixName(userName);
+
+// if (userNameFixed === null) { console.log(`nic nie wpisano`) }
+// else { console.log(`Imie ${userNameFixed} rozpoczyna sie od litery ${userNameFixed[0]}`); }
+// }
+
+const userFile = prompt("Podaj nazwe pliku wraz z rozserzeniem");
+const returnFile = fileInfo(userFile);
+
+if (returnFile) {
+    console.log(returnFile);
+} else {
+    console.log("Nie podano rozserzenia z '.'");
 }
-const userNameFixed = fixName(userName);
 
-if (userNameFixed === null) { console.log(`nic nie wpisano`) }
-else { console.log(`Imie ${userNameFixed} rozpoczyna sie od litery ${userNameFixed[0]}`); }
+function fileInfo(file) {
 
+    if (!file.includes(".")) return false;
+    const lastDotindex = file.lastIndexOf(".");
+
+    return {
+        name: file.slice(0, lastDotindex),
+        extension: file.slice(lastDotindex + 1)
+    };
+}
